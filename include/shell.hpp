87953c444ce19f7328c3945b019d33446112f36d
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-05-11 17:10:15 +0200

    Add shell parser of argv

--- include/shell.hpp
+++ include/shell.hpp
@@ -3,6 +3,10 @@
 class Shell 
 {
     private:
+        double _cookingTime = 0.f;
+        size_t _numberOfCooks = 0;
+        size_t _timeToRefillMs = 0;
+
     public:
         Shell(int ac, char **argv);
         void run(void);
--- src/shell.cpp
+++ src/shell.cpp
@@ -1,11 +1,26 @@
 #include <string>
 #include <iostream>
+#include <string>
+#include <regex>
+#include <format>
 
 #include "shell.hpp"
 
 Shell::Shell(__attribute_maybe_unused__ int ac,__attribute_maybe_unused__ char **argv)
 {
-    /*fill info*/
+    try
+    {
+        _cookingTime = std::stod(argv[1]);
+        _numberOfCooks = std::stoi(argv[2]);
+        _timeToRefillMs = std::stoi(argv[3]);
+        std::cout << std::format("- Time (double) : {}\n", _cookingTime);
+        std::cout << std::format("- Number of Cooks (int)    : {}\n", _numberOfCooks);
+        std::cout << std::format("- Time to refill the kitchens (int)    : {}\n", _timeToRefillMs);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
 
 void Shell::run()
@@ -16,5 +31,4 @@ void Shell::run()
     {
         std::cout << line << std::endl;
     }
-    
 }
