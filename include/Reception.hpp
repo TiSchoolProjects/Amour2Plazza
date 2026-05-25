143e0936858dde469245dbef3bd0be30b9ecba26
titouanhct <titouan.hochet@epitech.eu>
2026-05-25 16:32:47 +0200

    Add Reception

--- include/Reception.hpp
+++ include/Reception.hpp
@@ -1,1 +1,36 @@
 #pragma once
+
+#include <vector>
+#include <utility>
+#include <string>
+#include <unistd.h>
+#include "Kitchen.hpp"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "EnumPlazza.hpp"
+#include "Ipc.hpp"
+
+class Reception {
+    public:
+        Reception(double cookingTime, size_t numberOfCooks, size_t timeToRefillMs);
+        ~Reception();
+        void handleOrder(const std::vector<std::pair<PizzaType, PizzaSize>> &pizzas);
+        void displayStatus();
+        void update();
+
+    private:
+        struct KitchenInfo {
+            pid_t pid;
+            int id;
+            size_t activePizzas;
+
+        };
+        double _cookingTime;
+        size_t _numberOfCooks;
+        size_t _timeToRefillMs;
+        int _nextKitchenId;
+        std::vector<KitchenInfo> _kitchens;
+        Ipc _ipc;
+        void createKitchen();
+        void logPizzaReady(PizzaType type, PizzaSize size, int kitchenId);
+};
