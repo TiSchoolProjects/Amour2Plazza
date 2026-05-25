317ddadd3ea1363e8f3939fbaab323a06ce2dc0f
eiffis <enzo.iffis@epitech.eu>
2026-05-26 00:13:23 +0200

    [ADD] reception unique ptr to shell

--- include/shell.hpp
+++ include/shell.hpp
@@ -5,13 +5,14 @@
 
 #include "EnumPlazza.hpp"
 #include <string>
+#include "Reception.hpp"
 
 class Shell {
 private:
   double _cookingTime = 0.f;
   size_t _numberOfCooks = 0;
   size_t _timeToRefillMs = 0;
-  
+  std::unique_ptr<Reception> _reception;
   const std::unordered_map<std::string, PizzaType> _pizzaTypeMap;
   const std::unordered_map<std::string, PizzaSize> _pizzaSizeMap;
 
