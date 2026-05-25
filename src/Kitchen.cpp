2efd55f70d56baff403cc00fc9a1d7daf1b9a485
eiffis <enzo.iffis@epitech.eu>
2026-05-25 22:46:48 +0200

    [ADD] cookfct method implemented

---
+++ src/Kitchen.cpp
@@ -0,0 +1,65 @@
+#include "Kitchen.hpp"
+
+Kitchen::Kitchen(double cookingTime, int numberOfCooks, int timeToRefillMS, int id) : _ipc(true)
+{
+    _cookingTime = cookingTime;
+    _numberOfCooks = numberOfCooks;
+    _timeToRefillMS = timeToRefillMS;
+    _nbOfPizzas = 0;
+    _isActive = true;
+    _id = id;
+
+    for (size_t i = 0; i < 8; i++) {
+        Ingredients ing = static_cast<Ingredients>(i);
+        _stock.push_back({ing, 5});
+    }
+    for (size_t i = 0; i < numberOfCooks; i++) {
+        _cooks.push_back(std::make_unique<Thread<std::function<void()>>>([this]() {
+            this->cookFct();
+        }));
+    }
+    for (auto &cook : _cooks) {
+        cook->startThread();
+    }
+}
+
+Kitchen::~Kitchen()
+{
+    for (auto &cook : _cooks) {
+        cook->stopThread();
+    }
+}
+
+void Kitchen::cookFct()
+{
+    std::pair<PizzaType, PizzaSize> currentOrder;
+    double bakeTime = 0.0;
+
+    while(_isActive) {
+        _mutex.lock();
+        if (!_orders.empty()) {
+            currentOrder = _orders.front();
+            _orders.pop();
+            _mutex.unlock();
+            switch (currentOrder.first){
+                case Regina :
+                    bakeTime = 2.0;
+                    break;
+                case Margarita : 
+                    bakeTime = 1.0;
+                    break;
+                case Americana :
+                    bakeTime = 2.0;
+                    break;
+                case Fantasia :
+                    bakeTime = 4.0;
+                    break;
+            }
+            double total = bakeTime * _cookingTime;
+            std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(total * 1000)));
+            _ipc.sendPizzaToReception(currentOrder.first, currentOrder.second, _id);
+        } else {
+            _mutex.unlock();
+        }
+    }
+}
