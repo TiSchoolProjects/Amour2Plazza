ee60b3b21d07caa53abc9f8aea1bb15fc3effdfd
eiffis <enzo.iffis@epitech.eu>
2026-05-25 19:45:30 +0200

    [ADD] Kitchen class, with std::vector of Thread for cooks, queue for oerders and vector of std pair for the stock of ingredients

--- include/Kitchen.hpp
+++ include/Kitchen.hpp
@@ -1,1 +1,36 @@
 #pragma once
+#include "Reception.hpp"
+#include "Thread.hpp"
+#include <queue>
+#include <functional>
+
+enum Ingredients 
+{
+    dough,
+    tomato,
+    gruyere,
+    ham,
+    mushrooms,
+    steak,
+    eggPlant,
+    goatCheese,
+    chiefLove
+};
+
+class Kitchen 
+{
+    public:
+        Kitchen(double cookingTime, int numberOfCooks, int timeToRefillMS);
+        ~Kitchen();
+        void run();
+    private:
+        std::vector<std::pair<Ingredients, int>> _stock;
+        int _numberOfCooks;
+        std::vector<std::unique_ptr<Thread<std::function<void()>>>> _cooks;
+        std::queue<std::pair<PizzaType, PizzaSize>> _orders;
+        Ipc _ipc;
+        int _id;
+        int _nbOfPizzas;
+        int _timeToRefillMS;
+        double _cookingTime;
+};
